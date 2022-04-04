5a4f36c532655f02d6a83cd7574b69e5f563003f
Mathieu <degracia.mathieu@gmail.com>
2022-04-04 23:30:32 +0200

    Add collect command

---
+++ app/Console/Commands/ClipCollect.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Dtos\Interval;
+use App\Jobs\StoreClip;
+use Carbon\CarbonPeriod;
+use Illuminate\Console\Command;
+use App\Services\IntervalFactory;
+use Illuminate\Support\Collection;
+use App\Managers\Twitch\TwitchManager;
+use App\Exceptions\ResponseIsEmptyException;
+
+class ClipCollect extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clip:collect {startedAt}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $intervals = $this->getIntervals();
+
+        foreach ($intervals as $interval) {
+
+            try {
+
+                $clips = $this->getClips($interval);
+
+                $clips->map(function ($clip) {
+                    StoreClip::dispatch($clip)->onQueue('clip-store');
+                });
+
+            } catch (ResponseIsEmptyException $e) {}
+        }
+
+        return Command::SUCCESS;
+    }
+
+    protected function getIntervals(): array
+    {
+        $period = $this->getPeriod();
+
+        $intervals = [];
+
+        foreach ($period as $startedAt) {
+
+            $endedAt = $startedAt->clone()->endOfMonth()->endOfDay();
+
+            $intervals[] = IntervalFactory::custom($startedAt, $endedAt);
+        }
+
+        return $intervals;
+    }
+
+    protected function getPeriod(): CarbonPeriod
+    {
+        $startedAt = $this->argument('startedAt');
+
+        return CarbonPeriod::create(
+            $startedAt, 
+            '1 month', 
+            Carbon::today(),
+        );
+    }
+
+    protected function getClips(Interval $interval): Collection
+    {
+        return app(TwitchManager::class)
+            ->driver('rawapi')
+            ->getClips($interval);
+    }
+}
